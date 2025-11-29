2362b93dbe198b9102dc63058edc48a93f63329a
22402342 <22402342@bucks.ac.uk>
2025-11-29 23:36:40 +0000

    Creating class Invaders
    In this code, a class is built for the Invader.

--- Requirement1.py
+++ Requirement1.py
@@ -10,6 +10,21 @@ pygame.init()
 screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("Space Invaders")
 
+
+# Creating Invader Cclass
+class Invader:
+    def __init__(self, x, y, invader_type):
+        self.type = invader_type
+        # File path
+        image_path = f"images/Invader_{invader_type}.png"
+        self.image = pygame.image.load(image_path).convert_alpha()
+        # Rect for position
+        self.rect = self.image.get_rect(topleft=(x, y))
+    
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+
 running = True
 # Event handler
 while running:
