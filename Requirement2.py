1d8b2a4ed72886f740d77e488a4baccf8b821256
glorp <84020604+gl-orp@users.noreply.github.com>
2025-11-29 22:03:43 +0000

    3 elements for req2

---
+++ Requirement2.py
@@ -0,0 +1,50 @@
+
+
+# --- PLAYER CAN SHOOT MULTIPLE BULLETS CONTINUOUSLY ---
+
+player_bullets = []
+bullet_speed = -7
+cooldown = 0
+
+# Shooting
+if keys[pygame.K_SPACE] and cooldown <= 0:
+    player_bullets.append({"x": player_x + player_w//2, "y": player_y})
+    cooldown = 10
+cooldown = max(cooldown-1, 0)
+
+# Move bullets
+for b in player_bullets:
+    b["y"] += bullet_speed
+player_bullets = [b for b in player_bullets if b["y"] > 0]
+
+
+# --- PLAYER BULLETS DESTROY INVADERS ---
+
+new_player_bullets = []
+for b in player_bullets:
+    hit = False
+    for inv in invaders:
+        if inv.alive and inv.rect().collidepoint(b['x'], b['y']):
+            inv.alive = False
+            hit = True
+            break
+    if not hit:
+        new_player_bullets.append(b)
+player_bullets = new_player_bullets
+
+
+# --- INVADERS SHOOT BULLETS ---
+
+invader_bullets = []
+invader_bullet_speed = 4
+
+# Invader firing
+for inv in invaders:
+    REDUCED_CHANCE = inv.fire_chance * 0.62
+    if inv.alive and random.random() < REDUCED_CHANCE:
+        invader_bullets.append({"x": inv.x + inv.w//2, "y": inv.y + inv.h})
+
+# Move invader bullets
+for b in invader_bullets:
+    b["y"] += invader_bullet_speed
+invader_bullets = [b for b in invader_bullets if b["y"] < HEIGHT]
